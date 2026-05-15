bcfb04eada4644aa919d53d26f97af6cf1efb166
Gino Filicetti <ginof@google.com>
2026-05-15 15:56:55 -0400

    added .env support to abstract out sensitive information

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,17 @@
-from anthropic import AnthropicVertex
+from anthropic import AnthropicVertex # type: ignore
+import os
+from dotenv import load_dotenv
 
-client = AnthropicVertex(region="global", project_id="YOUR_PROJECT_ID")
+load_dotenv()
+
+project_id = os.getenv("PROJECT_ID")
+region = os.getenv("REGION", "global")
+
+client = AnthropicVertex(region=region, project_id=project_id)
 message = client.messages.create(
  max_tokens=1024,
  messages=[{"role": "user", "content": "Hello! Can you help me?"}],
  model="claude-opus-4-6"
 )
 
-print(message.content[0].text)
+print(message.content[0].text) # type: ignore
